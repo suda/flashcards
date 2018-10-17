cac52fa37c00cd598d18d0fde77ea0f69cb5c846
Wojtek Siudzinski <admin@suda.pl>
2018-10-17 23:56:08 +0200

    Remove Firebase

--- src/App.js
+++ src/App.js
@@ -2,17 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import Card from './Card/Card';
 import DrawButton from './DrawButton/DrawButton';
-import firebase from 'firebase/app';
-import 'firebase/database';
-
-import { DB_CONFIG } from './Config/Firebase/db_config';
 
 class App extends Component {
   constructor(props){
     super(props);
 
-    this.app = firebase.initializeApp(DB_CONFIG);
-    this.database = this.app.database().ref().child('cards');
     this.updateCard = this.updateCard.bind(this);
 
     this.state = {
@@ -24,20 +18,19 @@ class App extends Component {
   componentWillMount(){
     console.log(this.app.database().ref().child('cards'))
     const currentCards = this.state.cards;
-    this.database.on('child_added', snap => {
-      currentCards.push({
-        id: snap.key,
-        eng: snap.val().eng,
-        han: snap.val().han,
-        pin: snap.val().pin,
-      })
 
-      this.setState({
-        cards: currentCards,
-        currentCard: this.getRandomCard(currentCards)
-      })
+    currentCards
+        .push({
+          id: 1,
+          eng: 'foo',
+          han: 'bar',
+          pin: 'baz',
+        });
 
-    })
+    this.setState({
+      cards: currentCards,
+      currentCard: this.getRandomCard(currentCards)
+    });
   }
 
   getRandomCard(currentCards){
