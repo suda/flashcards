04464ebf42a1a1d4b04d0a3d8f0696bba6afb4e1
Wojtek Siudzinski <admin@suda.pl>
2018-10-18 11:57:00 +0200

    Improve CSS

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -5,11 +5,15 @@ const Card = (props) => (
     <div className="card-container">
         <div className="card">
             <div className="front">
-                <div className="title">{props.data.title}</div>
-                <div className="subtitle">{props.data.subtitle}</div>
+                <div className="content">
+                    <div className="title">{props.data.title}</div>
+                    <div className="subtitle">{props.data.subtitle}</div>
+                </div>
             </div>
             <div className="front back">
-                <div className="text">{props.data.text}</div>
+                <div className="content">
+                    <div className="text">{props.data.text}</div>
+                </div>
             </div>
         </div>
     </div>
